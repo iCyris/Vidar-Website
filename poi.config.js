44ceec63e66a71ac1eaf7e6a59c9a7e5b7ddcc46
iCyris <cyris1015@gmail.com>
2019-06-02 21:12:02 +0800

    finish: member page

--- poi.config.js
+++ poi.config.js
@@ -2,5 +2,5 @@ module.exports = {
     entry: 'src/scripts/app.js',
     output: {
         publicUrl: "/"
-    } 
+    }
 }
--- src/scripts/components/Main/Member.jsx
+++ src/scripts/components/Main/Member.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import member_info from '../../config/member.js'
 
 export default (props) => {
 
+    const memberProfile = member_info.map((member) =>
+        <div className="profile" key={member.id}>
+            <a className="member__avatar" href={member.url} title={member.id} target="_blank">
+                <img title={member.id} src={member.avatar} />
+                <span>{member.id}</span>
+            </a>
+            <div className="member__intro">
+                <p>{member.intro}</p>
+            </div>
+        </div>
+    )
+
     return (
         <section className="_member">
-
+            <div className="member__line-start" />
+            <div className="member__background" />
+            <div className="member__content">
+                <h2>Member</h2>
+                {memberProfile}
+            </div>
+            <div className="member__line-end" />
         </section>
     )
 }
