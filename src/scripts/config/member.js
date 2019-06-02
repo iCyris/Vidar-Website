6becb29a99c6d1339c0c56fb27b19cd6e61aa072
iCyris <cyris1015@gmail.com>
2019-06-02 23:24:38 +0800

    fix: font blurred

--- src/scripts/config/member.js
+++ src/scripts/config/member.js
@@ -7,7 +7,7 @@ export default [
     },
     {
         "id": "Edward_L",
-        "intro": "13 级 HDUISA 会长／Web 安全／华为安全部门",
+        "intro": "13 级 HDUISA 会长 / Web 安全 / 华为安全部门",
         "avatar": require("../../images/avatar/edward_L.jpg"),
         "url": "/#"
     },
@@ -31,49 +31,49 @@ export default [
     },
     {
         "id": "Explorer",
-        "intro": "14 级 HDUISA 会长／二进制安全／曾被 NULL、TD、蓝莲花等拉去凑人数／差点没毕业／长亭安研",
+        "intro": "14 级 HDUISA 会长 / 二进制安全 / 曾被 NULL、TD、蓝莲花等拉去凑人数 / 差点没毕业 / 长亭安研",
         "avatar": require("../../images/avatar/Explorer.jpg"),
         "url": "/#"
     },
     {
         "id": "Aklis",
-        "intro": "14 级 HDUISA 副会长／二次元／拼多多安全工程师",
+        "intro": "14 级 HDUISA 副会长 / 二次元 / 拼多多安全工程师",
         "avatar": require("../../images/avatar/aklis.jpg"),
         "url": "/#"
     },
     {
         "id": "Sysorem",
-        "intro": "14 级网安协会会长／HDUISA 成员／Web 安全／Freebuf 安全社区特约作者／FSI2015Freebuf 特邀嘉宾",
+        "intro": "14 级网安协会会长 / HDUISA 成员 / Web 安全 / Freebuf 安全社区特约作者 / FSI2015Freebuf 特邀嘉宾",
         "avatar": require("../../images/avatar/sysorem.jpg"),
         "url": "/#"
     },
     {
         "id": "Hcamael",
-        "intro": "Web 安全／知道创宇 404 安全实验室／IOT、Web、二进制漏洞全能研究",
+        "intro": "Web 安全 / 知道创宇 404 安全实验室 / IOT、Web、二进制漏洞全能研究",
         "avatar": require("../../images/avatar/hcamael.jpg"),
         "url": "/#"
     },
     {
         "id": "LoRexxar",
-        "intro": "14 级／Web 🐶／杭电江流儿／自走棋主教守门员",
+        "intro": "14 级 / Web 🐶 / 杭电江流儿 / 自走棋主教守门员",
         "avatar": require("../../images/avatar/lorexxar.jpg"),
         "url": "https://lorexxar.cn/"
     },
     {
         "id": "A1ex",
-        "intro": "14 级网安协会副会长／Web 安全",
+        "intro": "14 级网安协会副会长 / Web 安全",
         "avatar": require("../../images/avatar/alex.jpg"),
         "url": "/#"
     },
     {
         "id": "Ahlaman",
-        "intro": "14 级网安协会副会长／无线安全",
+        "intro": "14 级网安协会副会长 / 无线安全",
         "avatar": require("../../images/avatar/ahlaman.jpg"),
         "url": "/#"
     },
     {
         "id": "Undefinedv",
-        "intro": "15 级网安协会会长／Web 安全",
+        "intro": "15 级网安协会会长 / Web 安全",
         "avatar": require("../../images/avatar/undefinedv.jpg"),
         "url": "/#"
     },
@@ -85,7 +85,7 @@ export default [
     },
     {
         "id": "Spine",
-        "intro": "逆向／二进制安全",
+        "intro": "逆向 / 二进制安全",
         "avatar": require("../../images/avatar/spine.jpg"),
         "url": "/#"
     },
@@ -97,31 +97,31 @@ export default [
     },
     {
         "id": "逆风",
-        "intro": "13 级菜鸡／大数据打杂",
+        "intro": "13 级菜鸡 / 大数据打杂",
         "avatar": require("../../images/avatar/deadwind4.jpeg"),
         "url": "https://github.com/deadwind4"
     },
     {
         "id": "Airbasic",
-        "intro": "Web 安全／长亭科技安服部门／TSRC 2015 年年度英雄榜第八、2016 年年度英雄榜第十三",
+        "intro": "Web 安全 / 长亭科技安服部门 / TSRC 2015 年年度英雄榜第八、2016 年年度英雄榜第十三",
         "avatar": require("../../images/avatar/airbasic.jpg"),
         "url": "/#"
     },
     {
         "id": "jibo",
-        "intro": "15 级／什么都不会的开发／打什么都菜",
+        "intro": "15 级 / 什么都不会的开发 / 打什么都菜",
         "avatar": require("../../images/avatar/jibo.jpg"),
         "url": "/#"
     },
     {
         "id": "Processor",
-        "intro": "15 级 Vidar 会长／送分型逆向选手／半年更博客博主／13 段剑纯",
+        "intro": "15 级 Vidar 会长 / 送分型逆向选手 / 半年更博客博主 / 13 段剑纯",
         "avatar": require("../../images/avatar/Processor.jpeg"),
         "url": "https://processor.pub/"
     },
     {
         "id": "HeartSky",
-        "intro": "15 级／挖不到洞／打不动 CTF／内网渗透不了／工具写不出",
+        "intro": "15 级 / 挖不到洞 / 打不动 CTF / 内网渗透不了 / 工具写不出",
         "avatar": require("../../images/avatar/heartsky.jpg"),
         "url": "http://heartsky.info"
     },
@@ -133,235 +133,235 @@ export default [
     },
     {
         "id": "c014",
-        "intro": "15 级／Web 🐶／汪汪汪",
+        "intro": "15 级 / Web 🐶 / 汪汪汪",
         "avatar": require("../../images/avatar/c014.png"),
         "url": "/#"
     },
     {
         "id": "Junier",
-        "intro": "16 级／打杂／摸鱼／立志学术的研狗／经济之光／R 语言忠实粉丝",
+        "intro": "16 级 / 打杂 / 摸鱼 / 立志学术的研狗 / 经济之光 / R 语言忠实粉丝",
         "avatar": require("../../images/avatar/Junier.jpg"),
         "url": "/#"
     },
     {
         "id": "NeverMoes",
-        "intro": "16 级／Java 福娃／上班 996／下班 669",
+        "intro": "16 级 / Java 福娃 / 上班 996 / 下班 669",
         "avatar": require("../../images/avatar/nervermoes.jpg"),
         "url": "/#"
     },
     {
         "id": "Sora",
-        "intro": "16 级／Web Developer",
+        "intro": "16 级 / Web Developer",
         "avatar": require("../../images/avatar/Sora.jpg"),
         "url": "https://github.com/Last-Order"
     },
     {
         "id": "fantasyqt",
-        "intro": "16 级／摸鱼选手／可能会运维",
+        "intro": "16 级 / 摸鱼选手 / 可能会运维",
         "avatar": require("../../images/avatar/fantasyqt.jpg"),
         "url": "http://0x2f.xyz"
     },
     {
         "id": "vvv_347",
-        "intro": "16 级／Freelancer",
+        "intro": "16 级 / Freelancer",
         "avatar": require("../../images/avatar/vvv_347.png"),
         "url": "https://vvv-347.space"
     },
     {
         "id": "veritas501",
-        "intro": "16 级／Bin／被迫研狗",
+        "intro": "16 级 / Bin / 被迫研狗",
         "avatar": require("../../images/avatar/veritas501.jpeg"),
         "url": "https://veritas501.space"
     },
     {
         "id": "LuckyCat",
-        "intro": "16 级／Web 🐱／现于长亭科技实习",
+        "intro": "16 级 / Web 🐱 / 现于长亭科技实习",
         "avatar": require("../../images/avatar/princessprincepal.jpg"),
         "url": "https://jianshu.com/u/ad5c1e097b84"
     },
     {
         "id": "Ash",
-        "intro": "16 级／Java 开发攻城狮／996 选手／濒临猝死",
+        "intro": "16 级 / Java 开发攻城狮 / 996 选手 / 濒临猝死",
         "avatar": require("../../images/avatar/ash.jpg"),
         "url": "/#"
     },
     {
         "id": "Cyris",
-        "intro": "16 级／Web 前端／不会设计／CSS 超可爱",
+        "intro": "16 级 / Web 前端 / 不会设计 / CSS 超可爱",
         "avatar": require("../../images/avatar/Cyris.jpg"),
         "url": "https://cyris.moe/"
     },
     {
         "id": "Acaleph",
-        "intro": "16 级／水母一小只／程序员鼓励师／Cy 来组饥荒惹！",
+        "intro": "16 级 / 水母一小只 / 程序员鼓励师 / Cy 来组饥荒惹！",
         "avatar": require("../../images/avatar/Acaleph.jpg"),
         "url": "/#"
     },
     {
         "id": "b0lv42",
-        "intro": "16 级／啥也不会／业余美工专业打杂／万年不写博／WOW 评级上分可找我（逃",
+        "intro": "16 级 / 啥也不会 / 业余美工专业打杂 / 万年不写博 / WOW 评级上分可找我（逃",
         "avatar": require("../../images/avatar/b0lv42.jpg"),
         "url": "https://b0lv42.github.io/"
     },
     {
         "id": "ngc7293",
-        "intro": "16 级／摸鱼选手／找不到工作，被迫考研",
+        "intro": "16 级 / 摸鱼选手 / 找不到工作，被迫考研",
         "avatar": require("../../images/avatar/ngc7293.jpg"),
         "url": "https://ngc7292.github.io/"
     },
     {
         "id": "ckj123",
-        "intro": "16 级／Web／菜鸡第一人",
+        "intro": "16 级 / Web / 菜鸡第一人",
         "avatar": require("../../images/avatar/ckj123.jpg"),
         "url": "https://www.ckj123.com"
     },
     {
         "id": "cru5h",
-        "intro": "16 级／Web",
+        "intro": "16 级 / Web",
         "avatar": require("../../images/avatar/cru5h.jpg"),
         "url": "/#"
     },
     {
         "id": "xiaoyao52110",
-        "intro": "16 级／Bin 打杂／他们说菜都是假的，我是真的",
+        "intro": "16 级 / Bin 打杂 / 他们说菜都是假的，我是真的",
         "avatar": require("../../images/avatar/xiaoyao52110.jpg"),
         "url": "/#"
     },
     {
         "id": "Li4n0",
-        "intro": "Web 安全爱好者／半个程序员／没有女朋友",
+        "intro": "Web 安全爱好者 / 半个程序员 / 没有女朋友",
         "avatar": require("../../images/avatar/li4no.jpg"),
         "url": "https://blog.0e1.top"
     },
     {
         "id": "f1rry",
-        "intro": "17 级／Web",
+        "intro": "17 级 / Web",
         "avatar": require("../../images/avatar/f1rry.png"),
         "url": "/#"
     },
     {
         "id": "mian",
-        "intro": "17 级／业余开发／专业摸鱼",
+        "intro": "17 级 / 业余开发 / 专业摸鱼",
         "avatar": require("../../images/avatar/mian.jpg"),
         "url": "https://www.intmian.com"
     },
     {
         "id": "ACce1er4t0r",
-        "intro": "17 级／Web 狗如何在险恶的 CTF 中存活／学习 & 摸鱼中",
+        "intro": "17 级 / Web 狗如何在险恶的 CTF 中存活 / 学习 & 摸鱼中",
         "avatar": require("../../images/avatar/ACce1er4t0r.jpg"),
         "url": "https://www.vctorcontrol.cn"
     },
     {
         "id": "MiGo",
-        "intro": "17 级／摸鱼怪／meaqua 箱推人／有人带我打游戏吗",
+        "intro": "17 级 / 摸鱼怪 / meaqua 箱推人 / 有人带我打游戏吗",
         "avatar": require("../../images/avatar/MiGo.jpg"),
         "url": "https://migoooo.github.io/"
     },
     {
         "id": "Insomnia",
-        "intro": "17 级／Web 安全／运维大师的小弟",
+        "intro": "17 级 / Web 安全 / 运维大师的小弟",
         "avatar": require("../../images/avatar/Insomnia.jpg"),
         "url": "https://brownfly.github.io"
     },
     {
         "id": "Aris",
-        "intro": "17 级／没有灵魂的 Pwn 手／key 厨",
+        "intro": "17 级 / 没有灵魂的 Pwn 手 / key 厨",
         "avatar": require("../../images/avatar/aris.jpg"),
         "url": "https://blog.ar1s.top"
     },
     {
         "id": "oyiadin",
-        "intro": "17 级／摸鱼选手／专业打杂／啥都不会／啥都不懂",
+        "intro": "17 级 / 摸鱼选手 / 专业打杂 / 啥都不会 / 啥都不懂",
         "avatar": require("../../images/avatar/oyy.jpg"),
         "url": "https://blog.b1n.top/"
     },
     {
         "id": "Lou00",
-        "intro": "17 级／Web／../",
+        "intro": "17 级 / Web / ../",
         "avatar": require("../../images/avatar/lou.jpg"),
         "url": "https://blog.lou00.top"
     },
     {
         "id": "wenzhuan",
-        "intro": "18 级／完全不会安全／一个做设计的鸽子美工／天天画表情包",
+        "intro": "18 级 / 完全不会安全 / 一个做设计的鸽子美工 / 天天画表情包",
         "avatar": require("../../images/avatar/wenzhuan.jpg"),
         "url": "https://wzyxv1n.top/"
     },
     {
         "id": "Cosmos",
-        "intro": "18 级／逆不了／Pwn 不出／尽梨了",
+        "intro": "18 级 / 逆不了 / Pwn 不出 / 尽梨了",
         "avatar": require("../../images/avatar/cosmos.jpg"),
         "url": "https://shcosmos.github.io"
     },
     {
         "id": "Y",
-        "intro": "18 级／Bin／CSGO 白给选手",
+        "intro": "18 级 / Bin / CSGO 白给选手",
         "avatar": require("../../images/avatar/Y.jpg"),
         "url": "https://blog.xyzz.ml:444/"
     },
     {
         "id": "Annevi",
-        "intro": "18 级／Web 底层",
+        "intro": "18 级 / Web 底层",
         "avatar": require("../../images/avatar/Annevi.jpg"),
         "url": "https://lwh.red"
     },
     {
         "id": "logong",
-        "intro": "18 级／盲人／真· Web 底层",
+        "intro": "18 级 / 盲人 / 真· Web 底层",
         "avatar": require("../../images/avatar/logong.jpg"),
         "url": "http://logong.vip"
     },
     {
         "id": "Kev1n",
-        "intro": "18 级／Web 菜鸡／只想知道赛博朋克 2077 什么时候发布",
+        "intro": "18 级 / Web 菜鸡 / 只想知道赛博朋克 2077 什么时候发布",
         "avatar": require("../../images/avatar/Kev1n.jpg"),
         "url": "https://blog.kev1n.top"
     },
     {
         "id": "LurkNoi",
-        "intro": "18 级／不是学姐／不务正业／MC 打杂",
+        "intro": "18 级 / 不是学姐 / 不务正业 / MC 打杂",
         "avatar": require("../../images/avatar/LurkNoi.jpg"),
         "url": "/#"
     },
     {
         "id": "幼稚园",
-        "intro": "18 级／Re／CS 只会干拉",
+        "intro": "18 级 / Re / CS 只会干拉",
         "avatar": require("../../images/avatar/youzhiyuan.jpg"),
         "url": "https://www.danisjiang.xyz/"
     },
     {
         "id": "rosetta",
-        "intro": "18 级／Java／这个人太菜了不配拥有简介",
+        "intro": "18 级 / Java / 这个人太菜了不配拥有简介",
         "avatar": require("../../images/avatar/rosetta.jpg"),
         "url": "http://47.107.239.93/"
     },
     {
         "id": "Roc826",
-        "intro": "18 级／Web 底层选手",
+        "intro": "18 级 / Web 底层选手",
         "avatar": require("../../images/avatar/Roc826.jpg"),
         "url": "http://www.roc826.cn/"
     },
     {
         "id": "Seadom",
-        "intro": "18 级／Web／真·菜到超乎想象／拼死学（mo）习（yu）中",
+        "intro": "18 级 / Web / 真·菜到超乎想象 / 拼死学（mo）习（yu）中",
         "avatar": require("../../images/avatar/Seadom.jpg"),
         "url": "/#"
     },
     {
         "id": "ObjectNotFound",
-        "intro": "18 级／Web 真·菜鸡，别人的菜都是假的／这个人懒到不写简介",
+        "intro": "18 级 / Web 真·菜鸡，别人的菜都是假的 / 这个人懒到不写简介",
         "avatar": require("../../images/avatar/ObjectNotFound.jpg"),
         "url": "https://www.zhouweitong.site/"
     },
     {
         "id": "Moesang",
-        "intro": "18 级／不擅长 Web／擅长摸鱼／摸鱼！",
+        "intro": "18 级 / 不擅长 Web / 擅长摸鱼 / 摸鱼！",
         "avatar": require("../../images/avatar/Moesang.png"),
         "url": "https://blog.wz22.cc"
     },
     {
         "id": "E99p1ant",
-        "intro": "18 级／开发菜鸡／Web 摸鱼选手",
+        "intro": "18 级 / 开发菜鸡 / Web 摸鱼选手",
         "avatar": require("../../images/avatar/E99p1ant.jpg"),
         "url": "https://github.red"
     }
